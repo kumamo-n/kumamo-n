946bbfe31ac278242a0334f63b72b17f18021cdc
kumamo-n <kmntmht116@gmail.com>
2019-12-28 07:24:24 +0900

    fix

--- src/components/Post/ShareButtons.vue
+++ src/components/Post/ShareButtons.vue
@@ -2,17 +2,17 @@
 
   <div class="share_buttons">
 
-    <a :href="`http://b.hatena.ne.jp/entry/${this.pagePath}`" class='socialButton3'>
+    <a :href="`http://b.hatena.ne.jp/entry/${this.pagePath}`" >
       <svg width="20px" height="40px" viewBox="0 0 141.732 141.732" enable-background="new 0 0 141.732 141.732">
         <use xlink:href="/hatena.svg#root" />
       </svg>
     </a>
-    <a :href="`http://twitter.com/share?url=${this.pagePath}&text=${this.title}`" class='socialButton2' target="_blank" rel="noopener">
+    <a :href="`http://twitter.com/share?url=${this.pagePath}&text=${this.title}`"  target="_blank" rel="noopener">
       <svg width="20px" height="40px"viewBox="0 0 20 16">
         <use xlink:href="/twitter.svg#root" />
       </svg>
     </a>
-    <a :href="`http://getpocket.com/edit?url=${this.pagePath}`" class='socialButton1' target="_blank" rel="noopener">
+    <a :href="`http://getpocket.com/edit?url=${this.pagePath}`"  target="_blank" rel="noopener">
       <svg width="20px" height="40px" viewBox="0 0 20 16">
         <use xlink:href="/pocket.svg#root" />
       </svg>
