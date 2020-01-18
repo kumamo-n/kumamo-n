3e456ba572120fa76057477eb1d07f209762756f
kumamo-n <kmntmht116@gmail.com>
2020-01-18 13:06:35 +0900

    fix

--- src/components/Post/ShareButtons.vue
+++ src/components/Post/ShareButtons.vue
@@ -2,17 +2,17 @@
 
   <div class="share_buttons">
 
-    <a :href="`http://b.hatena.ne.jp/entry/${this.pagePath}`" >
+    <a :href="`http://b.hatena.ne.jp/entry/${this.path}`" >
       <svg width="20px" height="40px" viewBox="0 0 141.732 141.732" enable-background="new 0 0 141.732 141.732">
         <use xlink:href="/hatena.svg#root" />
       </svg>
     </a>
-    <a :href="`http://twitter.com/share?url=${this.pagePath}&text=${this.title}`"  target="_blank" rel="noopener">
+    <a :href="`http://twitter.com/share?url=${this.path}&text=${this.title}`"  target="_blank" rel="noopener">
       <svg width="20px" height="40px"viewBox="0 0 20 16">
         <use xlink:href="/twitter.svg#root" />
       </svg>
     </a>
-    <a :href="`http://getpocket.com/edit?url=${this.pagePath}`"  target="_blank" rel="noopener">
+    <a :href="`http://getpocket.com/edit?url=${this.path}`"  target="_blank" rel="noopener">
       <svg width="20px" height="40px" viewBox="0 0 20 16">
         <use xlink:href="/pocket.svg#root" />
       </svg>
@@ -32,11 +32,6 @@ export default Vue.extend({
       title: {
         type:String
       }
-  },
-    computed: {
-        pagePath():string {
-            return this.path
-        }
-    }
+  }
 })
 </script>
